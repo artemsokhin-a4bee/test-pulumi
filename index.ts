3ab318a1930cd0b045779c2b8505dd781b6c855f
Artem <88371337+artem-sokhin-a4bee@users.noreply.github.com>
2023-05-31 16:13:05 +0200

    install node-fetch

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import * as pulumi from '@pulumi/pulumi'
 import * as aws from '@pulumi/aws'
 import * as fs from 'fs'
+import fetch from 'node-fetch'
 
 const s3Bucket = new aws.s3.Bucket('s3Bucket', {
   acl: 'private',
